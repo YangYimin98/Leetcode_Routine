b2b7f7afa634e6d63a8d3e728e98674bab0b9ce8
YiminYang980510 <67627410+YiminYang980510@users.noreply.github.com>
2021-08-25 23:25:39 +0200

    Create 剑指 Offer 17. 打印从1到最大的n位数.py

---
+++ Yimin_Yang/剑指 Offer 17. 打印从1到最大的n位数.py
@@ -0,0 +1,20 @@
+"""输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999"""
+
+
+
+class Solution:
+    def printNumbers(self, n: int) -> List[int]:
+        def dfs(ind, num, digit):
+            if ind == digit: 
+                temp.append(int(''.join(num))) 
+                return
+            for i in range(0, 10):
+                num.append(str(i))
+                dfs(ind + 1, num, digit)
+                num.pop()
+        temp = []
+        for i in range(1, n +1):
+            for j in range(1, 10):
+                num = [str(j)]
+                dfs(1, num, i)
+        return temp
